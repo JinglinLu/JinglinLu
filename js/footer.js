f470d36c91373deb2a4a94fe2633f52db283e9ee
JinglinLu <jinglinlu1113@gmail.com>
2024-05-15 13:34:14 -0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,1 @@
-$(function(){
-    $("#footer-placeholder").load("footer.html", function(response, status, xhr) {
-      if (status == "error") {
-        console.log("Error loading footer.html: " + xhr.status + " " + xhr.statusText);
-      }
-    });
-  });
-  
+$(function () {$("#footer-placeholder").load("/footer.html");});
