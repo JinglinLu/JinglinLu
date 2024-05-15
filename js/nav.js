6fad7d53c72efb77fe84a7b48ab46fad2cd0838c
JinglinLu <jinglinlu1113@gmail.com>
2024-05-15 13:33:59 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,1 @@
-$(function(){
-    $("#nav-placeholder").load("nav.html", function(response, status, xhr) {
-      if (status == "error") {
-        console.log("Error loading nav.html: " + xhr.status + " " + xhr.statusText);
-      }
-    });
-  });
-  
+$(function () {$("#nav-placeholder").load("/nav.html");});
